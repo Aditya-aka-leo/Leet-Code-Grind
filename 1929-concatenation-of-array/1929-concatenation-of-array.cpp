fcd6abc07a5a58f56fc7f1f4082d81d693074982
Aditya <72493902+Aditya-aka-leo@users.noreply.github.com>
2022-06-04 11:24:47 +0530

    Time: 15 ms (36.53%), Space: 12.5 MB (87.72%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -6,15 +6,8 @@ public:
         int j=0;
         vector<int>ans;
         
-        for (int i=0;i<nums.size();i++)
-        {
-                ans.push_back(nums[i]);
-        }
-         for (int i=0;i<nums.size();i++)
-        {
-                ans.push_back(nums[i]);
-        }
-        
+        ans.insert(ans.end(),nums.begin(),nums.end());
+        ans.insert(ans.end(),nums.begin(),nums.end());
         return ans;
     }
 };
