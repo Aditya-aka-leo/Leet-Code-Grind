d39e348616559c6f508dd39e1c78fc5be215aa19
Aditya <72493902+Aditya-aka-leo@users.noreply.github.com>
2022-06-05 23:42:10 +0530

    Time: 19 ms (6.14%), Space: 12.4 MB (68.07%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -4,14 +4,12 @@ public:
 {
         sort(nums.begin(), nums.end());
         int min=*nums.begin();
-        auto it=nums.begin();
-        while(it+1 !=nums.end())
-        it++;
-        int max=*it;
+        int max = nums.at(nums.size()-1);   
         int i=1;
         int temp=1;
         while(i <= min)
         {
+
             if(min%i==0 && max%i==0)
             {
                 if(i > temp)
