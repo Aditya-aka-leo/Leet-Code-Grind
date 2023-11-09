9563d5e82a320a77252666e19115f468b93404ff
Aditya <72493902+Aditya-aka-leo@users.noreply.github.com>
2023-11-10 00:13:09 +0530

    Added solution - LeetHub

---
+++ Sort first half in ascending and second half in descending - GFG/sort-first-half-in-ascending-and-second-half-in-descending.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution{
+  public:
+    void customSort(int arr[], int n) {
+        // code here   
+        vector<int> v;
+        for(int i =0 ; i <n;i++)
+        v.push_back(arr[i]);
+        sort(v.begin(),v.begin()+n/2);
+        sort(v.begin()+n/2,v.end(),greater<int>());
+       for(int i  =0 ;i<n;i++)
+       arr[i]=v[i];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        obj.customSort(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
